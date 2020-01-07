f2b7c950a16098ccd439a081737c8a1d1d32a674
Ritvick-me <pandey.ritvick.v@gmail.com>
2020-01-07 18:54:17 +0530

    First Home page push
    Nav and Footer attached.

---
+++ Resources/js/index.js
@@ -0,0 +1,187 @@
+let ele=document.getElementById("high");
+        let ele1=document.getElementById("A");
+        let ele2=document.getElementById("B");
+        let ele3=document.getElementById("C");
+        let ele4=document.getElementById("low");
+        let ele5=document.getElementById("D");
+        let ele6=document.getElementById("E");
+        let ele7=document.getElementById("F");
+        let ele8=document.getElementById("G");
+        let ele9=document.getElementById("Hi");
+        let ele10=document.getElementById("I");
+        let ele11=document.getElementById("J");
+        let ele100=document.getElementById("Z");
+        let ele12=document.getElementById("ko");
+        let ele13=document.getElementById("K");
+        let ele14=document.getElementById("L");
+        let ele15=document.getElementById("M");
+        let ele16=document.getElementById("N");
+        let ele17=document.getElementById("ro");
+        let ele18=document.getElementById("O");
+        let ele19=document.getElementById("P");
+        let ele20=document.getElementById("Q");
+        
+        
+
+
+        ele.onmouseover=function(){
+            setTimeout(function(){
+                ele1.style.background="#CEAD3A";
+                ele1.style.color="white";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        ele3.style.background="#CEAD3A";
+                        ele3.style.color="white";
+                    },80);
+                    ele2.style.background="#CEAD3A";
+                    ele2.style.color="white";
+                },150);
+            },180);
+        };
+        ele.onmouseout=function(){
+            setTimeout(function(){
+                ele3.style.background="transparent";
+                ele3.style.color="black";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        ele1.style.background="transparent";
+                        ele1.style.color="black";
+                    },180);
+                    ele2.style.background="transparent";
+                    ele2.style.color="black";
+                },150);
+            },80);
+        };
+        
+        ele4.onmouseover=function(){
+            setTimeout(function(){
+                ele5.style.background="#CEAD3A";
+                ele5.style.color="white";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        setTimeout(function(){
+                            ele8.style.background="#CEAD3A";
+                            ele8.style.color="white";
+                        },80);
+                        ele7.style.background="#CEAD3A";
+                        ele7.style.color="white";
+                    },150);
+                    ele6.style.background="#CEAD3A";
+                    ele6.style.color="white";
+                },180);
+            },200);
+        };
+        ele4.onmouseout=function(){
+            setTimeout(function(){
+                ele8.style.background="transparent";
+                ele8.style.color="black";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        setTimeout(function(){
+                            ele5.style.background="transparent";
+                            ele5.style.color="black";
+                        },80);
+                        ele6.style.background="transparent";
+                        ele6.style.color="black";
+                    },150);
+                    ele7.style.background="transparent";
+                    ele7.style.color="black";
+                },180);
+            },200);    
+        };
+        
+        ele9.onmouseover=function(){
+            setTimeout(function(){
+                ele10.style.background="#CEAD3A";
+                ele10.style.color="white";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        ele100.style.background="#CEAD3A";
+                        ele100.style.color="white";
+                    },80);
+                    ele11.style.background="#CEAD3A";
+                    ele11.style.color="white";
+                },150);
+            },180);
+        };
+        ele9.onmouseout=function(){
+            setTimeout(function(){
+                ele100.style.background="transparent";
+                ele100.style.color="black";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        ele10.style.background="transparent";
+                        ele10.style.color="black";
+                    },80);
+                    ele11.style.background="transparent";
+                    ele11.style.color="black";
+                },150);
+            },180);
+        };
+        
+        ele12.onmouseover=function(){
+            setTimeout(function(){
+                ele13.style.background="#CEAD3A";
+                ele13.style.color="white";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        setTimeout(function(){
+                            ele16.style.background="#CEAD3A";
+                            ele16.style.color="white";
+                        },80);
+                        ele15.style.background="#CEAD3A";
+                        ele15.style.color="white";
+                    },150);
+                    ele14.style.background="#CEAD3A";
+                    ele14.style.color="white";
+                },180);
+            },200);
+        };
+        ele12.onmouseout=function(){
+            setTimeout(function(){
+                ele16.style.background="transparent";
+                ele16.style.color="black";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        setTimeout(function(){
+                            ele13.style.background="transparent";
+                            ele13.style.color="black";
+                        },80);
+                        ele14.style.background="transparent";
+                        ele14.style.color="black";
+                    },150);
+                    ele15.style.background="transparent";
+                    ele15.style.color="black";
+                },180);
+            },200);
+        };
+
+        ele17.onmouseover=function(){
+            setTimeout(function(){
+                ele18.style.background="#CEAD3A";
+                ele18.style.color="white";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        ele20.style.background="#CEAD3A";
+                        ele20.style.color="white";
+                    },80);
+                    ele19.style.background="#CEAD3A";
+                    ele19.style.color="white";
+                },150);
+            },180);
+        };
+        ele17.onmouseout=function(){
+            setTimeout(function(){
+                ele20.style.background="transparent";
+                ele20.style.color="black";
+                setTimeout(function(){
+                    setTimeout(function(){
+                        ele18.style.background="transparent";
+                        ele18.style.color="black";
+                    },180);
+                    ele19.style.background="transparent";
+                    ele19.style.color="black";
+                },150);
+            },80);
+        };
+          
